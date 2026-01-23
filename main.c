fc1e31a2df529efa8b37c580c067e09d316a58fd
Mari Ice <mueshen.sh@gmail.com>
2026-01-23 21:38:33 +0300

    updated main.c to show somewhat representative use of the meowbj

--- main.c
+++ main.c
@@ -6,11 +6,8 @@
 int main(void)
 {
 	printf("hello world \n");  
-	meowObj *cube=loadObj("../../plane.obj");
+	meowObj *cube=loadObj("pathHere.obj");
 	
-	FILE *debugFile;
-	debugFile=fopen("debugFile.obj", "wb");
-
 	if (cube == NULL ){
 		fclose(debugFile);
 		printf("loading failed, exit code\n");
@@ -19,14 +16,10 @@ int main(void)
 	}
 	
 	int faceCount= cube->stats.faces;
-	for(int i=0; i<faceCount*3;i++){
-		fprintf(debugFile," v %f,%f,%f   \n",cube->vert[i].x,cube->vert[i].y,cube->vert[i].z);
-
-	}
-	meowStats fileStats=cube->stats;
+f	meowStats fileStats=cube->stats;
 	printf(" stats: \nv:%zu, n:%zu, t:%zu, faces:%zu\n",fileStats.vert,fileStats.norm,fileStats.texc,fileStats.faces);
 	
-	fclose(debugFile);
+	(debugFile);
 	freeObj(cube);
 	return 0;
 }
